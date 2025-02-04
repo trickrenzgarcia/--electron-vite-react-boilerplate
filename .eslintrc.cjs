02d3d0dc2ab9168a30351f17cfa41b033eda2169
rwick <trickrenzgarcia@gmail.com>
2025-02-05 05:32:35 +0800

    --init

---
+++ .eslintrc.cjs
@@ -0,0 +1,12 @@
+const { rules } = require('eslint-plugin-react')
+
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    '@electron-toolkit/eslint-config-ts/recommended',
+    '@electron-toolkit/eslint-config-prettier'
+  ],
+  ...rules
+}
---
+++ src/renderer/src/pages/App.tsx
@@ -0,0 +1,10 @@
+
+function App(): JSX.Element {
+  return (
+    <>
+      <h1>Electron + Vite React + Shadcn UI (Boilerplate)</h1>
+    </>
+  )
+}
+
+export default App
